b1fa09a0910d9ca1f03c0c6b9d665393edd7de04
OLIVER <OLIVER@DESKTOP-F0KCEJ4>
2026-04-26 23:19:32 -0500

    Primer cambio

---
+++ CONNECTED/src/main/java/pe/edu/upc/connected/ConnectedApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.connected;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectedApplication.class, args);
+    }
+
+}
---
+++ CONNECTED/src/test/java/pe/edu/upc/connected/ConnectedApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.connected;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
