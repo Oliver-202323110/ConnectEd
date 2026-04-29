667e62b3097bda47ceccfe8bf97a2a29515025a7
Rodrigo Ismael Viton Trelles <149454425+Roxugo@users.noreply.github.com>
2026-04-28 21:11:14 -0500

    Update Publicacion.java
    creacion tabla

--- CONNECTED/src/main/java/pe/edu/upc/connected/entities/Publicacion.java
+++ CONNECTED/src/main/java/pe/edu/upc/connected/entities/Publicacion.java
@@ -1,4 +1,10 @@
 package pe.edu.upc.connected.entities;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Publicacion")
 public class Publicacion {
 }
