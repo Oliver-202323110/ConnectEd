f3779d52ed1616c1fdf40d7ba1b02ecdfdb1aa38
Rodrigo Ismael Viton Trelles <149454425+Roxugo@users.noreply.github.com>
2026-04-29 19:36:48 -0500

    creacion listID de usuario
    creacion listID de usuario para la utilizacion dentro del crud de publicacion

--- CONNECTED/src/main/java/pe/edu/upc/connected/servicesimplements/UsuarioServiceImplement.java
+++ CONNECTED/src/main/java/pe/edu/upc/connected/servicesimplements/UsuarioServiceImplement.java
@@ -7,6 +7,7 @@ import pe.edu.upc.connected.repositories.IUsuarioRepository;
 import pe.edu.upc.connected.servicesinterfaces.IUsuarioService;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioServiceImplement implements IUsuarioService {
@@ -16,4 +17,9 @@ public class UsuarioServiceImplement implements IUsuarioService {
 
    @Override
    public List<Usuario> list() { return UsuarioRepositorio.findAll(); }
+
+   @Override
+   public Optional<Usuario> listId(int id) {
+      return UsuarioRepositorio.findById(id);
+   }
 }
